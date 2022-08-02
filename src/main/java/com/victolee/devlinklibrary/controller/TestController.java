4d4b1604dea02adb29f4378bfd98230e41b68372
victolee <victolee93@gmail.com>
2022-08-02 20:28:22 +0900

    init

--- src/main/java/com/victolee/devlinklibrary/controller/TestController.java
+++ src/main/java/com/victolee/devlinklibrary/controller/TestController.java
@@ -1,4 +1,14 @@
 package com.victolee.devlinklibrary.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
 public class TestController {
+    @GetMapping("/")
+    public String list() {
+        return "foo";
+    }
 }
